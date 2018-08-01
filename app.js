165c9a59defd20bdc0349ac6bc561e8a2cc41a94
Mary Brady <emceebrady@gmail.com>
2018-08-01 13:21:58 -0600

    project setup

---
+++ app.js
@@ -0,0 +1,13 @@
+function play(playerChoice) {
+    var computerChoice = 'rock'
+    
+    if (computerChoice === 5) {
+
+    } else if (computerChoice > 4) {
+
+    } else if (computerChoice < 2) {
+
+    } else {
+    }
+}
+
