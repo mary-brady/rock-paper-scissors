64133bb7808c052df90d8172a62386d258f8d223
Mary Brady <emceebrady@gmail.com>
2018-08-01 15:00:36 -0600

    scissors button fix + math.random fixes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let choices = ['rock', 'paper', 'scissors']
 
 function play(playerChoice) {
     var computerChoice = makeChoice();
-    if (computerChoice == playerChoice){
+    if (computerChoice == playerChoice) {
         return window.alert("Computer chose " + computerChoice + ", it's a tie!")
     }
     else if (computerChoice === 'rock') {
@@ -12,22 +12,13 @@ function play(playerChoice) {
         else {
             return window.alert("Computer chose " + computerChoice +  ", you lose!")
         }
-        //if player chose paper
-            //player wins
-        //else 
-            //player loses
-
     } else if (computerChoice == 'paper') {
             if (playerChoice == 'scissors') {
                 return window.alert("Computer chose " + computerChoice + ", you win!")
-
             } else {
                 return window.alert("Computer chose " + computerChoice + ", you lose!")
             }
-        //if player chose scissors
-            //player win
-        //else
-            //player lose
+
     } else {
         if (playerChoice == 'rock') {
             return window.alert("Computer Chose " + computerChoice + ", you win!")
@@ -35,14 +26,10 @@ function play(playerChoice) {
         else {
             return window.alert("Computer Chose " + computerChoice + ", you lose!")
         }
-        //if player chose rock
-            //player win
-        //else 
-             //player lose
-}
+    }
 }
 
 
 function makeChoice() {
-    return choices[Math.floor(Math.random() * Math.floor(2))];
+    return choices[Math.floor(Math.random() * Math.floor(3))];
 } 
